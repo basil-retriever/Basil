df9a61f91a5b3b13f890f90c90927335706f3c31
Niels Jacobs <jacobsniels10@gmail.com>
2025-07-14 09:31:26 +0200

    initial

--- basil-search/app.py
+++ basil-search/app.py
@@ -8,6 +8,5 @@ from routers import ask, detect_intent, generate_ticket
 app = FastAPI(title="BasilApi", version="1.0")
 
 app.include_router(detect_intent.router)
-app.include_router(generate_ticket.router)
 app.include_router(ask.router)
 app.include_router(site_scanner.router)
